07801a9d1fb21a112d25fe41c47f46ddb3833b04
alvaro <alvaro.yapu.cossio@gmail.com>
2021-05-04 13:21:41 -0400

    Feature #6: Add menu icons

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Movies from './src/components/Movies';
-import Series from './src/components/Series';
-import Explore from './src/components/Explore';
-
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Movies from "./src/components/Movies";
+import Series from "./src/components/Series";
+import Explore from "./src/components/Explore";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Movies} />
-        <Tab.Screen name="Series" component={Series} />
-        <Tab.Screen name="Explore" component={Explore} />
+        <Tab.Screen
+          name="Movies"
+          component={Movies}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="film" color={color} size={size} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Series"
+          component={Series}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="tv" color={color} size={size} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Explore"
+          component={Explore}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="globe" color={color} size={size} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
@@ -23,8 +49,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
