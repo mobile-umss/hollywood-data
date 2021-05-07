8f71dff90a6dfd9c1d8d153f291c025345983c18
alvaro <alvaro.yapu.cossio@gmail.com>
2021-05-06 22:23:59 -0400

    Feature #11: Add getSeries method

---
+++ src/api/Series.ts
@@ -0,0 +1,20 @@
+import api from "../api/http-commons";
+import { Serie } from "../models/series";
+
+export default class SeriesAPI {
+  private static instance: SeriesAPI;
+
+  private constructor() {}
+
+  static getInstance(): SeriesAPI {
+    if (!SeriesAPI.instance) {
+      SeriesAPI.instance = new SeriesAPI();
+    }
+    return SeriesAPI.instance;
+  }
+
+  async getPopular(): Promise<Serie[]> {
+    let series: Serie[] = await (await api.get("/tv/popular")).data.results
+    return series
+  }
+}
--- src/components/Series.tsx
+++ src/components/Series.tsx
@@ -1,14 +1,26 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import SeriesAPI from "../api/Series";
+import { Serie } from "../models/series";
 
-export class Series extends Component {
+export class Series extends Component<{}, { title: String }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      title: "",
+    };
+  }
+  async componentDidMount() {
+    let series: Serie[] = await SeriesAPI.getInstance().getPopular();
+    this.setState({ title: series.map((serie) => serie.name).toString() });
+  }
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text> Series </Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text> {this.state.title} </Text>
       </View>
-    )
+    );
   }
 }
 
-export default Series
+export default Series;
