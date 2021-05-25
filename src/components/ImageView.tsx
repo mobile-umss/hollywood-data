9e27e968311dfc4a47ec703111dbec05313586ab
Alvaro <alvaro.yapu.cossio@gmail.com>
2021-05-25 14:09:52 -0400

    Feature #9: Add carrousel

---
+++ src/components/ImageView.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Dimensions } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+
+interface ImageViewProps {
+  url: string;
+}
+const { width: screenWidth } = Dimensions.get('window')
+
+export default class ImageView extends Component<ImageViewProps, {}> {
+  render() {
+    const fullUrl = "https://image.tmdb.org/t/p/w500" + this.props.url;
+    return <Image style={styles.poster} source={{ uri: fullUrl }} />;
+  }
+}
+const styles = StyleSheet.create({
+  poster: {
+    width: 100,
+    height: 150,
+    resizeMode: "cover",
+  },
+});
--- src/screens/Movies.tsx
+++ src/screens/Movies.tsx
@@ -1,32 +1,164 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import {
+  Dimensions,
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  Platform,
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import Carousel, {
+  AdditionalParallaxProps,
+  ParallaxImage,
+} from "react-native-snap-carousel";
 import MovieAPI from "../api/Movies";
+import ImageView from "../components/ImageView";
 import { Movie } from "../models/movie";
 
+interface MovieState {
+  popularMovies: Movie[];
+  upcomingMovies: Movie[];
+  topRatedMovies: Movie[];
+  loading: boolean;
+  activeIndex: number;
+}
+interface RenderItem {
+  item: Movie;
+  index: number;
+  parallaxProps: any;
+}
+interface ParallaxProps {
+  parallaxProps?: AdditionalParallaxProps;
+}
 
+const { width: screenWidth } = Dimensions.get("window");
 
-export class Movies extends Component<{}, { title: String }> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      title: '',
-    };
-  }
+export default class Movies extends Component<{}, MovieState> {
+  _carousel: any;
+  state = {
+    popularMovies: new Array<Movie>(),
+    upcomingMovies: new Array<Movie>(),
+    topRatedMovies: new Array<Movie>(),
+    loading: true,
+    activeIndex: 0,
+  };
 
- 
   async componentDidMount() {
-    let movies: Movie[] = await MovieAPI.getInstance().getUpcoming();
-    this.setState({ title: movies.map( movie => movie.original_title ).toString()})
+    let popularMovies: Movie[] = await MovieAPI.getInstance().getPopular();
+    let topRatedMovies: Movie[] = await MovieAPI.getInstance().getTopRated();
+    let upcomingMovies: Movie[] = await MovieAPI.getInstance().getUpcoming();
+    this.setState({
+      popularMovies,
+      topRatedMovies,
+      upcomingMovies,
+      loading: false,
+    });
   }
- 
-  render() {
-    const auxTitle = this.state.title;
+
+  _renderItem = (
+    { item, index }: RenderItem,
+    { parallaxProps }: ParallaxProps
+  ): any => {
+    console.log(item.original_title);
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text> {this.state.title} </Text>
+      <View
+        style={{
+          borderRadius: 5,
+          height: 0,
+          padding: 50,
+        }}
+      >
+        <ParallaxImage
+          source={{ uri: item.poster_path }}
+          containerStyle={styles.imageContainer}
+          style={styles.image}
+          parallaxFactor={0.4}
+          {...parallaxProps}
+        />
+        <ImageView url={item.poster_path} />
       </View>
+    );
+  };
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text> Loading</Text>
+        </View>
+      );
+    }
+
+    return (
+      <ScrollView>
+        <Text style={styles.text}>Popular</Text>
+        <View style={{ marginTop: 0, height: 200 }}>
+          <Carousel
+            layout={"default"}
+            ref={(ref) => (this._carousel = ref)}
+            data={this.state.popularMovies}
+            sliderWidth={300}
+            sliderHeight={300}
+            itemHeight={screenWidth - 60}
+            itemWidth={120}
+            renderItem={this._renderItem}
+            hasParallaxImages={true}
+          />
+        </View>
+        <Text style={styles.text}>Top Rated</Text>
+        <View style={{ marginTop: 0, height: 200 }}>
+          <Carousel
+            layout={"default"}
+            ref={(ref) => (this._carousel = ref)}
+            data={this.state.topRatedMovies}
+            sliderWidth={300}
+            sliderHeight={300}
+            itemHeight={screenWidth - 60}
+            itemWidth={120}
+            renderItem={this._renderItem}
+            hasParallaxImages={true}
+          />
+        </View>
+        <Text style={styles.text}>Upcoming</Text>
+        <View style={{ marginTop: 0, height: 200 }}>
+          <Carousel
+            layout={"default"}
+            ref={(ref) => (this._carousel = ref)}
+            data={this.state.upcomingMovies}
+            sliderWidth={300}
+            sliderHeight={300}
+            itemHeight={screenWidth - 60}
+            itemWidth={120}
+            renderItem={this._renderItem}
+            hasParallaxImages={true}
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
 
-export default Movies;
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: "cover",
+  },
+  item: {
+    width: 0,
+    height: 0,
+  },
+  imageContainer: {
+    flex: 1,
+    marginBottom: Platform.select({ ios: 0, android: 1 }), // Prevent a random Android rendering issue
+    backgroundColor: "white",
+    borderRadius: 8,
+  },
+  text: {
+    fontSize: 18,
+    paddingLeft: 10,
+  },
+});
