44ab1b232242f70b528ac27d97be47087bc27ddd
VicenteR <vicentestefano4042unknowedtrue@gmail.com>
2021-06-14 19:04:30 -0400

    Feature #11: Add getSeries method with Scrollview

--- src/screens/SeriesDetailsScreen.tsx
+++ src/screens/SeriesDetailsScreen.tsx
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 import { Text, View, StyleSheet, Image } from "react-native";
 import { Serie } from "../models/series";
+import { ScrollView } from "react-native-gesture-handler";
 
 
 export default function SeriesDetailsScreen ({route}) {
     const { item } = route.params;
     const imgUrl = "https://image.tmdb.org/t/p/w500" + item.poster_path;
     return(
-        <View>
+        <ScrollView>
             <Image style = {style.poster} source = {{ uri: imgUrl}} ></Image>
             <Text style={style.text}> TITLE:{item.name} </Text>
             <Text style={style.text}> FIRST AIR DATE: {item.first_air_date}  </Text>
             <Text style={style.text}> POPULARITY: {item.popularity} </Text>
             <Text style={style.text}> OVERVIEW: {item.overview} </Text>
-        </View>
+        </ScrollView>
     );
 }
 
@@ -22,8 +23,8 @@ export default function SeriesDetailsScreen ({route}) {
 
 const style = StyleSheet.create({
     poster:{
-        width: 200,
-        height: 350,
+        width: 300,
+        height: 450,
         resizeMode: "cover",
     },
     text:{
