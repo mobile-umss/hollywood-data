4a8b1d671fe4f4496783861b32883d8d981d1bc5
VicenteR <vicentestefano4042unknowedtrue@gmail.com>
2021-06-20 21:36:34 -0400

    Feature #23: Add ItemLayout to Series Working the Layout was modified to fix some problems reading style information for the user interface, like an option

---
+++ src/components/ItemLayout.tsx
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import {
+  Image,
+  ImageBackground,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  StyleSheet,
+} from "react-native";
+import { color } from "react-native-reanimated";
+
+interface ItemLayoutProps {
+  title: string;
+  poster_path: string;
+  release_date: string;
+  popularity: number;
+  overview: string;
+}
+
+const { width, height } = Dimensions.get("window");
+
+export class ItemLayout extends Component<ItemLayoutProps, {}> {
+  render() {
+    const title = this.props.title;
+    const imgUrl = this.props.poster_path;
+    const release_date = this.props.release_date;
+    const popularity = this.props.popularity;
+    const overview = this.props.overview;
+    return (  
+      //<ImageBackground source={{ uri: imgUrl }} style={styles.image}>
+        <ScrollView style={styles.bottomContainer}>
+                <Image style = {styles.image} source = {{ uri: imgUrl}} ></Image>                
+               
+                <View style={styles.flexRow}>
+                    <Text style={[styles.textBlack]}>{release_date}</Text>
+                </View>
+                <View>
+                    <Text style={[styles.textWhite, styles.title, styles.textCenter, styles.textBackGBlack]}>{title}</Text>
+                    <View style={[styles.flexRow, styles.flexEnd]}>
+                    <Text style={[styles.textBlack, styles.popularity, styles.textBackGRed]}>
+                        {popularity}
+                    </Text>
+                    </View>
+                    <Text style={[styles.textBlack, styles.desc, styles.textJustif]} numberOfLines={36}>
+                    {overview}
+                    </Text>
+                </View>
+        </ScrollView>
+      //</ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  topContainer:{ height: (height - 150) / 2, width },
+  image: {
+    width: 300,
+    height: 450,
+    resizeMode: "cover",
+    flex: 1,
+    //width,
+    //height: height - 130,
+    //position: "absolute",
+    top: 0,
+    left: 30,
+    zIndex: 5,
+  },
+  bottomContainer: {
+    flex: 1,
+    paddingHorizontal: 25,
+    position: "relative",
+    zIndex: 10,
+  },
+  flexRow: { flexDirection: "row" },
+  textWhite: { color: "#D6D6D6" },
+  textBlack: { color: "#000000" },
+  textJustif:{textAlign:'justify'},
+  textCenter:{textAlign:'center'},
+  textBackGRed:{backgroundColor: 'red'},
+  textBackGBlack:{backgroundColor: 'black'},
+  flexEnd: { alignItems: "flex-end" },
+  popularity: { fontSize: 18 },
+  desc: { marginTop: 10 },
+  title: {
+    fontSize: 48,
+    textTransform: "uppercase",
+    lineHeight: 56,
+    marginVertical: 20,
+  },
+});
+
+export default ItemLayout;
--- src/screens/SeriesDetailsScreen.tsx
+++ src/screens/SeriesDetailsScreen.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ItemLayout from '../components/ItemLayout';
 import { Text, View, StyleSheet, Image } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 
@@ -7,19 +8,27 @@ export default function SeriesDetailsScreen ({route}) {
     const { item } = route.params;
     const imgUrl = "https://image.tmdb.org/t/p/w500" + item.poster_path;
     return(
-        <ScrollView>
+        <ItemLayout
+        title={item.name}
+        poster_path={imgUrl}
+        release_date={item.first_air_date}
+        popularity={item.popularity}
+        overview={item.overview}
+      />
+       
+        /* <ScrollView>
             <Image style = {style.poster} source = {{ uri: imgUrl}} ></Image>
             <Text style={style.text}> TITLE:{item.name} </Text>
             <Text style={style.text}> FIRST AIR DATE: {item.first_air_date}  </Text>
             <Text style={style.text}> POPULARITY: {item.popularity} </Text>
             <Text style={style.text}> OVERVIEW: {item.overview} </Text>
-        </ScrollView>
+        </ScrollView>*/
     );
 }
 
 
 
-
+/*
 const style = StyleSheet.create({
     poster:{
         width: 300,
@@ -30,5 +39,5 @@ const style = StyleSheet.create({
 
     }
 })
-
+*/
 
