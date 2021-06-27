eda54447e63e39a97dd52fe42d70f68505adcd58
VicenteR <vicentestefano4042unknowedtrue@gmail.com>
2021-06-27 13:31:21 -0400

    Adding description and Info. in About

--- src/navigation/AboutNavigator.tsx
+++ src/navigation/AboutNavigator.tsx
@@ -5,7 +5,25 @@ export class AboutNavigator extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text> Prueba XD </Text>
+                
+                <Text style={[
+                    styles.textJustif,
+                    styles.textBackGBlack,
+                    styles.textWhite,
+                    styles.fontAbout,
+                    ]}> 
+                  This application is aimed at users who 
+                  want to know important information about
+                  movies and series, indicating their genre,
+                  overview, release dates and popularity.
+                  In addition to including the categories:
+                  Popular, Top Rated, Upcoming and Airing Today. 
+                  This application was developed by:
+                    {'\n'}          -Pablo Pardo.
+                    {'\n'}          -Vicente Rico.
+                    {'\n'}          -Alvaro Yapu. 
+
+                </Text>
             </View>
         )
     }
@@ -13,8 +31,14 @@ export class AboutNavigator extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 21
-  }
+    paddingTop: 150,
+
+  },
+  textWhite: { color: "#D6D6D6" },
+  textCenter: { textAlign: "center" },
+  textJustif: { textAlign: "justify" },
+  textBackGBlack: { backgroundColor: "black" },
+  fontAbout: {fontSize: 24},
 });
 
 export default AboutNavigator
